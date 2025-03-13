a5c93a1c591d70e43ce764b6ebde40e528003409
SachinSharma22 <s830sharma@gmail.com>
2025-03-14 00:30:21 +0530

    learn IIFE function

--- functionPractice/script.js
+++ functionPractice/script.js
@@ -21,8 +21,55 @@ const greet = (myName) => {
 console.log(greet('sachin'));
 */
 
-const greet = myName => `Hello, ${myName}!`;
+// const greet = myName => `Hello, ${myName}!`;
 
-console.log(greet('sharma'));
+// console.log(greet('sharma'));
+
+/*
+const obj = {
+    name: 'sachin',
+    regularFunction: function() {
+        console.log(this.name);
+    },
+    arrowFunction: () => {
+        console.log(this.name);
+    }
+};
+
+obj.regularFunction();
+obj.arrowFunction();
+*/
+
+// (IIFE) Immediately Invoked Function Expressions
+
+(function() {
+    const secret = 'I am an IIFE';
+    console.log(secret);
+})();
+
+
+// another example
+const counter = (function() {
+    let count = 0;
+    return{
+        increment: function() {
+            count++;
+        },
+        decrement: function() {
+            count--;
+        },
+        getCount: function() {
+            return count;
+        }
+    };
+})();
+
+counter.increment();
+counter.increment();
+console.log(counter.getCount());
+counter.decrement();
+console.log(counter.getCount());
+
+
 
 
