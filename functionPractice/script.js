97bef3be160f37c38eb9fda4805d2bc5688d7172
SachinSharma22 <s830sharma@gmail.com>
2025-03-14 00:42:32 +0530

    function for fibonacci series

--- functionPractice/script.js
+++ functionPractice/script.js
@@ -83,4 +83,19 @@ console.log(gen.next().value);
 console.log(gen.next().value);
 console.log(gen.next().value);
 
+// fibonacciGenerator
+function* fibonacciGenerator() {
+let a = 0, b = 1;
+while(true){
+    yield a;
+    [a, b] = [b, a+b];
+}
+}
+
+const fib = fibonacciGenerator();
+for(let i = 0; i<10; i++) {
+    console.log(fib.next().value);
+}
+
+
 
