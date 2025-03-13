c615e677ac880e43b2686cb95deb036a0d4dbeb2
SachinSharma22 <s830sharma@gmail.com>
2025-03-14 00:34:32 +0530

    generator function

--- functionPractice/script.js
+++ functionPractice/script.js
@@ -70,6 +70,17 @@ console.log(counter.getCount());
 counter.decrement();
 console.log(counter.getCount());
 
+// Generator Functions: Yieldig Results
 
+function* numberGenerator() {
+    yield 1;
+    yield 2;
+    yield 3;
+}
+
+const gen = numberGenerator();
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
 
 
